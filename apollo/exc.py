b10d46e1f9096fd3da2616b513157154546789db
Chady Kamar <chadykamar@users.noreply.github.com>
2021-08-11 22:18:28 -0400

    Define ParserException

--- apollo/exc.py
+++ apollo/exc.py
@@ -1,6 +1,16 @@
+from tok.tok import Token
+
+
 class UnexpectedCharacter(Exception):
     pass
 
 
 class UnterminatedString(Exception):
     pass
+
+
+class ParseException(Exception):
+
+    def __init__(self, msg, token: Token):
+        super().__init__(msg)
+        self.token = token
