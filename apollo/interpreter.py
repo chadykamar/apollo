55251206121fd381a94bd5aba5b06cf1dfc717ea
Chady Kamar <chadykamar@users.noreply.github.com>
2021-08-13 20:49:25 -0400

    Implement basic interpreter framework

---
+++ apollo/interpreter.py
@@ -0,0 +1,58 @@
+
+from token import NUMBER
+
+from exc import RuntimeException
+from expression import Binary, Expression, Grouping, Literal, Ternary, Unary
+from tok.type import TokenType as tt
+
+
+class Interpreter:
+
+    def interpret(self, expr: Expression):
+        return self.evaluate(expr)
+
+    def evaluate(self, expr: Expression):
+
+        match expr:
+            case Unary() as expr: return self.unary(expr)
+            case Binary() as expr: return self.binary(expr)
+            case Literal() as expr: return self.literal(expr)
+            case Grouping() as expr: return self.grouping(expr)
+            case Ternary() as expr: return self.ternary(expr)
+
+    def literal(self, expr: Literal):
+        return expr.value
+
+    def unary(self, expr: Unary):
+        right = self.evaluate(expr.right)
+
+        match expr.operator.type:
+            case tt.MINUS:
+                return -right
+            case tt.NOT:
+                return not bool(right)
+
+    def ternary(self, expr: Ternary):
+        return self.evaluate(expr.left) if self.evaluate(expr.condition) else self.evaluate(expr.right)
+
+    def grouping(self, expr: Grouping):
+        return self.evaluate(expr.expression)
+
+    def binary(self, expr: Binary):
+        left = self.evaluate(expr.left)
+        right = self.evaluate(expr.right)
+
+        try:
+            match expr.operator.type:
+                case tt.MINUS: return left - right
+                case tt.SLASH: return left / right
+                case tt.STAR: return left * right
+                case tt.PLUS: return left + right
+                case tt.GREATER: return left > right
+                case tt.GEQUAL: return left >= right
+                case tt.LESSER: return left < right
+                case tt.LEQUAL: return left <= right
+                case tt.EQUAL: return left == right
+                case tt.NEQUAL: return left != right
+        except (TypeError, ZeroDivisionError) as e:
+            raise RuntimeException(str(e), expr.operator)
