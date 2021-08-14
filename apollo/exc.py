191d6404be12c8f5c9f3d90db36cb63142e093b2
Chady Kamar <chadykamar@users.noreply.github.com>
2021-08-13 20:45:59 -0400

    Define RuntimeException

--- apollo/exc.py
+++ apollo/exc.py
@@ -9,8 +9,15 @@ class UnterminatedString(Exception):
     pass
 
 
-class ParseException(Exception):
-
+class ApolloException(Exception):
     def __init__(self, msg, token: Token):
         super().__init__(msg)
         self.token = token
+
+
+class ParseException(ApolloException):
+    pass
+
+
+class RuntimeException(ApolloException):
+    pass
