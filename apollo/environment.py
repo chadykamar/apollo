96ed0f718ad92b78ff36f3e762a999f4f87274cf
Chady Kamar <chadykamar@users.noreply.github.com>
2021-08-15 23:33:57 -0400

    Subclass dict rather that UserDict in Environment

--- apollo/environment.py
+++ apollo/environment.py
@@ -3,7 +3,10 @@ from tok.tok import Token
 from collections import UserDict
 
 
-class Environment(UserDict):
+class Environment(dict):
+
+    def __init__(self):
+        super().__init__()
 
     def __missing__(self, key):
         raise NameNotFoundException(f"name '{key}' is not defined")
