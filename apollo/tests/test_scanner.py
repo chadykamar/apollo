cafac207415761fdfe35d9ee01f60c67781d7653
Chady Kamar <chadykamar@users.noreply.github.com>
2021-12-30 12:56:33 -0500

    Add while loop test to scanner

--- apollo/tests/test_scanner.py
+++ apollo/tests/test_scanner.py
@@ -8,10 +8,10 @@ from tok import TokenType as tt
 def test_scanner_hello_world():
     expected = [
         Token(tt.IDENTIFIER, 1, "print"),
-        Token(tt.LPAREN, 1, '('),
+        Token(tt.LPAREN, 1, "("),
         Token(tt.STRING, 1, '"hello, world!"', "hello, world!"),
-        Token(tt.RPAREN, 1, ')'),
-        Token(tt.EOF, 1)
+        Token(tt.RPAREN, 1, ")"),
+        Token(tt.EOF, 1),
     ]
 
     scanner = Scanner('print("hello, world!")')
@@ -23,36 +23,36 @@ def test_scanner_hello_world():
 def test_symbols():
 
     expected = [
-        Token(tt.NEWLINE, 1, '\n'),
-        Token(tt.LPAREN, 2, '('),
-        Token(tt.LBRACE, 2, '{'),
-        Token(tt.LBRACK, 2, '['),
-        Token(tt.LESSER, 2, '<'),
-        Token(tt.GREATER, 2, '>'),
-        Token(tt.RBRACK, 2, ']'),
-        Token(tt.RBRACE, 2, '}'),
-        Token(tt.RPAREN, 2, ')'),
-        Token(tt.NEWLINE, 2, '\n'),
-        Token(tt.NEWLINE, 3, '\n'),
-        Token(tt.NEWLINE, 4, '\n'),
-        Token(tt.NEWLINE, 5, '\n'),
-        Token(tt.PLUS, 6, '+'),
-        Token(tt.MINUS, 6, '-'),
-        Token(tt.STAR, 6, '*'),
-        Token(tt.SLASH, 6, '/'),
-        Token(tt.PERCENT, 6, '%'),
-        Token(tt.DOT, 6, '.'),
-        Token(tt.COMMA, 6, ','),
-        Token(tt.SCOLON, 6, ';'),
-        Token(tt.COLON, 6, ':'),
-        Token(tt.NEQUAL, 6, '!='),
-        Token(tt.ASSIGN, 6, '='),
-        Token(tt.BANG, 6, '!'),
-        Token(tt.EQUAL, 6, '=='),
-        Token(tt.LEQUAL, 6, '<='),
-        Token(tt.GEQUAL, 6, '>='),
-        Token(tt.NEWLINE, 6, '\n'),
-        Token(tt.EOF, 7)
+        Token(tt.NEWLINE, 1, "\n"),
+        Token(tt.LPAREN, 2, "("),
+        Token(tt.LBRACE, 2, "{"),
+        Token(tt.LBRACK, 2, "["),
+        Token(tt.LESSER, 2, "<"),
+        Token(tt.GREATER, 2, ">"),
+        Token(tt.RBRACK, 2, "]"),
+        Token(tt.RBRACE, 2, "}"),
+        Token(tt.RPAREN, 2, ")"),
+        Token(tt.NEWLINE, 2, "\n"),
+        Token(tt.NEWLINE, 3, "\n"),
+        Token(tt.NEWLINE, 4, "\n"),
+        Token(tt.NEWLINE, 5, "\n"),
+        Token(tt.PLUS, 6, "+"),
+        Token(tt.MINUS, 6, "-"),
+        Token(tt.STAR, 6, "*"),
+        Token(tt.SLASH, 6, "/"),
+        Token(tt.PERCENT, 6, "%"),
+        Token(tt.DOT, 6, "."),
+        Token(tt.COMMA, 6, ","),
+        Token(tt.SCOLON, 6, ";"),
+        Token(tt.COLON, 6, ":"),
+        Token(tt.NEQUAL, 6, "!="),
+        Token(tt.ASSIGN, 6, "="),
+        Token(tt.BANG, 6, "!"),
+        Token(tt.EQUAL, 6, "=="),
+        Token(tt.LEQUAL, 6, "<="),
+        Token(tt.GEQUAL, 6, ">="),
+        Token(tt.NEWLINE, 6, "\n"),
+        Token(tt.EOF, 7),
     ]
 
     scanner = Scanner("\n({[<>]})\n\n\n\n+-*/%.,;:!==! ==<=>=\n")
@@ -65,24 +65,24 @@ def test_symbols():
 def test_keywords():
 
     expected = [
-        Token(tt.IF, 1, 'if'),
-        Token(tt.ELIF, 1, 'elif'),
-        Token(tt.ELSE, 1, 'else'),
-        Token(tt.TRUE, 1, 'True'),
-        Token(tt.FALSE, 1, 'False'),
-        Token(tt.NONE, 1, 'None'),
-        Token(tt.OR, 1, 'or'),
-        Token(tt.AND, 1, 'and'),
-        Token(tt.NOT, 1, 'not'),
-        Token(tt.CLASS, 1, 'class'),
-        Token(tt.SELF, 1, 'self'),
-        Token(tt.RETURN, 1, 'return'),
-        Token(tt.DEF, 1, 'def'),
-        Token(tt.IMPORT, 1, 'import'),
-        Token(tt.FOR, 1, 'for'),
-        Token(tt.WHILE, 1, 'while'),
-        Token(tt.DO, 1, 'do'),
-        Token(tt.EOF, 1)
+        Token(tt.IF, 1, "if"),
+        Token(tt.ELIF, 1, "elif"),
+        Token(tt.ELSE, 1, "else"),
+        Token(tt.TRUE, 1, "True"),
+        Token(tt.FALSE, 1, "False"),
+        Token(tt.NONE, 1, "None"),
+        Token(tt.OR, 1, "or"),
+        Token(tt.AND, 1, "and"),
+        Token(tt.NOT, 1, "not"),
+        Token(tt.CLASS, 1, "class"),
+        Token(tt.SELF, 1, "self"),
+        Token(tt.RETURN, 1, "return"),
+        Token(tt.DEF, 1, "def"),
+        Token(tt.IMPORT, 1, "import"),
+        Token(tt.FOR, 1, "for"),
+        Token(tt.WHILE, 1, "while"),
+        Token(tt.DO, 1, "do"),
+        Token(tt.EOF, 1),
     ]
 
     scanner = Scanner(
@@ -104,7 +104,7 @@ def test_unexpected_char():
 
 def test_identifier():
 
-    expected = [Token(tt.IDENTIFIER, 1, 'a'), Token(tt.EOF, 1)]
+    expected = [Token(tt.IDENTIFIER, 1, "a"), Token(tt.EOF, 1)]
 
     scanner = Scanner("a")
     tokens = scanner.scan_tokens()
@@ -123,7 +123,7 @@ def test_comment():
     assert tokens == expected
 
 
-@pytest.mark.parametrize('string', ["'hello'", '"hello"'])
+@pytest.mark.parametrize("string", ["'hello'", '"hello"'])
 def test_string(string):
 
     expected = [Token(tt.STRING, 1, string, string[1:-1]), Token(tt.EOF, 1)]
@@ -142,13 +142,12 @@ def test_unterminated_string():
         scanner.scan_tokens()
 
 
-@pytest.mark.parametrize('num', ["255", '1.5'])
+@pytest.mark.parametrize("num", ["255", "1.5"])
 def test_number(num):
 
     expected = [
-        Token(tt.NUMBER, 1, num,
-              float(num) if '.' in num else int(num)),
-        Token(tt.EOF, 1)
+        Token(tt.NUMBER, 1, num, float(num) if "." in num else int(num)),
+        Token(tt.EOF, 1),
     ]
 
     scanner = Scanner(num)
@@ -160,10 +159,10 @@ def test_number(num):
 def test_assignment():
 
     expected = [
-        Token(tt.IDENTIFIER, 1, 'hello'),
-        Token(tt.ASSIGN, 1, '='),
-        Token(tt.NUMBER, 1, '1234', 1234),
-        Token(tt.EOF, 1)
+        Token(tt.IDENTIFIER, 1, "hello"),
+        Token(tt.ASSIGN, 1, "="),
+        Token(tt.NUMBER, 1, "1234", 1234),
+        Token(tt.EOF, 1),
     ]
 
     scanner = Scanner("hello = 1234")
@@ -178,7 +177,7 @@ def test_indent():
         Token(tt.NEWLINE, 1, "\n"),
         Token(tt.IF, 2, "if"),
         Token(tt.TRUE, 2, "True"),
-        Token(tt.COLON, 2, ':'),
+        Token(tt.COLON, 2, ":"),
         Token(tt.NEWLINE, 2, "\n"),
         Token(tt.INDENT, 3, "    "),
         Token(tt.IDENTIFIER, 3, "print"),
@@ -186,11 +185,10 @@ def test_indent():
         Token(tt.STRING, 3, '"hello"', "hello"),
         Token(tt.RPAREN, 3, ")"),
         Token(tt.DEDENT, 3),
-        Token(tt.EOF, 3)
+        Token(tt.EOF, 3),
     ]
 
-    text = \
-"""
+    text = """
 if True:
     print("hello")"""
 
@@ -215,7 +213,7 @@ def test_dedent():
         Token(tt.INDENT, 3, "    "),
         Token(tt.IF, 3, "if"),
         Token(tt.TRUE, 3, "True"),
-        Token(tt.COLON, 3, ':'),
+        Token(tt.COLON, 3, ":"),
         Token(tt.NEWLINE, 3, "\n"),
         Token(tt.INDENT, 4, "        "),
         Token(tt.IDENTIFIER, 4, "print"),
@@ -229,16 +227,39 @@ def test_dedent():
         Token(tt.PLUS, 5, "+"),
         Token(tt.NUMBER, 5, "1", 1),
         Token(tt.NEWLINE, 5, "\n"),
-        Token(tt.EOF, 6)
+        Token(tt.EOF, 6),
     ]
 
-    text = \
-"""
+    text = """
 for i in range(10):
     if True:
         print("hello")
 1 + 1
 """
+
+    scanner = Scanner(text)
+
+    assert scanner.scan_tokens() == expected
+
+
+def test_while():
+
+    expected = [
+        Token(tt.WHILE, 1, "while"),
+        Token(tt.TRUE, 1, "True"),
+        Token(tt.COLON, 1, ":"),
+        Token(tt.NEWLINE, 1, "\n"),
+        Token(tt.INDENT, 2, "    "),
+        Token(tt.IDENTIFIER, 2, "print"),
+        Token(tt.LPAREN, 2, "("),
+        Token(tt.STRING, 2, '"hello"', "hello"),
+        Token(tt.RPAREN, 2, ")"),
+        Token(tt.DEDENT, 2),
+        Token(tt.EOF, 2),
+    ]
+
+    text = """while True:
+    print("hello")"""
 
     scanner = Scanner(text)
 
